9cc8a563361e7cc21b4224a6e97a2680dfe4d97a
lo <215226467+compile-inn@users.noreply.github.com>
2025-12-17 16:26:46 +0900

    JSON process added

--- hearsay/hearsay.js
+++ hearsay/hearsay.js
@@ -1,6 +1,10 @@
 // use it to get the chuncks of the main text
 async function getCorpus(text_file) {
- let corpus = await fetch(text_file);
- let corpusText = await corpus.text();
- document.getElementById("hearsayTest").innerHTML = corpusText
+    let corpus = await fetch(text_file);
+    let corpusText = JSON.parse(corpus)
+    //let corpusText = await corpus.text();
+    let array_len = corpusText.length
+    let index = Math.random() * array_len
+    let hearsay = corpusText[index]
+    document.getElementById("hearsayTest").innerHTML = hearsay
 }
