54e1c70e72e2e032452c2ce80ee097be9497cad6
lo <215226467+compile-inn@users.noreply.github.com>
2025-12-17 16:56:45 +0900

    bug fix

--- hearsay/hearsay.js
+++ hearsay/hearsay.js
@@ -1,10 +1,11 @@
 // use it to get the chuncks of the main text
-async function getCorpus(text_file) {
-    let corpus = await fetch(text_file);
+async function getCorpus(json_file) {
+    let file = await fetch(json_file);
+    let corpus = JSON.stringify(file)
     let corpusText = JSON.parse(corpus)
     //let corpusText = await corpus.text();
     let array_len = corpusText.length
-    let index = Math.random() * array_len
+    let index = Math.floor(Math.random() * array_len)
     let hearsay = corpusText[index]
     document.getElementById("hearsayTest").innerHTML = hearsay
 }
