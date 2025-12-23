72680a065790fb33beecd749272e4bb912522bca
lo <215226467+compile-inn@users.noreply.github.com>
2025-12-23 21:48:49 +0900

    new function get line

--- hearsay/hearsay.js
+++ hearsay/hearsay.js
@@ -4,10 +4,15 @@ async function getCorpus(jsonFile) {
     return corpusArray;
 }
 
+// Creates a random index to access an element of the corpus
+function getIndex(corpusArray) {
+    let arrayLen = corpusArray.length;
+    return Math.floor(Math.random() * arrayLen);
+}
+
 function makeHearsay(corpusArray) {
     // add further settings option later
     const arrayLen = corpusArray.length;
-    // let hearsayLen = 4;
     let hearsayLen = document.getElementById("hearsayLenUser").value;
     let separator = document.getElementById("separatorUser").value;
     let hearsay = "";
@@ -19,14 +24,32 @@ function makeHearsay(corpusArray) {
     return hearsay.substring(0, hearsayCutPoint); // removes hanging separator at the end of the string
 }
 
+function getLine(corpusArray) {
+    let index = getIndex(corpusArray);
+    let line = corpusArray[index];
+    return line
+}
+
 function displayHearsay(jsonFile) {
   getCorpus(jsonFile).then(
   function(value) {
             let hearsay = makeHearsay(value);
-            document.getElementById("hearsayTest").innerHTML = hearsay;
+            document.getElementById("wizardQuote").innerHTML = hearsay;
   },
   function(error) {
             console.error('Error fetching corpus:', error);
-            document.getElementById("hearsayTest").innerHTML = "Error loading text";}
+            document.getElementById("wizardQuote").innerHTML = "Error loading text";}
+);
+}
+
+function displayLine(jsonFile) {
+  getCorpus(jsonFile).then(
+  function(value) {
+            let line = getLine(value);
+            document.getElementById("wizardQuote").innerHTML = line;
+  },
+  function(error) {
+            console.error('Error fetching corpus:', error);
+            document.getElementById("wizardQuote").innerHTML = "Error loading text";}
 );
 }
