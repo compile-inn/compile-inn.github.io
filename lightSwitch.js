a53b3e2fecd61588f1ebe0a90632b838a13bcc97
lo <215226467+compile-inn@users.noreply.github.com>
2026-01-02 11:28:46 +0900

    ligtht switch modif

--- lightSwitch.js
+++ lightSwitch.js
@@ -1,5 +1,6 @@
 function lightSwitch() {
-    currentTheme = sessionStorage.getItem("colorTheme");
+    let currentTheme = sessionStorage.getItem("colorTheme");
+
     if (currentTheme == "light-mode") {
         document.documentElement.classList.remove('light-mode');
         sessionStorage.setItem("colorTheme", "");
@@ -9,10 +10,3 @@ function lightSwitch() {
     }
 
 }
-
-// Theme persistence script (place in <head> or as an external JS file)
-document.addEventListener('DOMContentLoaded', () => {
-  // Retrieve saved theme preference
-  sessionColorTheme = sessionStorage.getItem("colorTheme");
-  document.documentElement.classList.add(sessionColorTheme);
-});
