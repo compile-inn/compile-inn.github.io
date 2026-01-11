b3f233cf84ba9e6d7288b7f11f9868385b19fe8e
lo <215226467+compile-inn@users.noreply.github.com>
2026-01-11 14:39:40 +0900

    new theme added

--- lightSwitch.js
+++ lightSwitch.js
@@ -1,11 +1,18 @@
 function lightSwitch() {
     let currentTheme = localStorage.getItem("colorTheme");
-
-    if (currentTheme == "light-mode") {
-        document.documentElement.classList.remove('light-mode');
-        localStorage.setItem("colorTheme", "");
-    } else {
-        document.documentElement.classList.add('light-mode');
-        localStorage.setItem("colorTheme", "light-mode");
+    switch (currentTheme) {
+        case "":
+            document.documentElement.classList.add('light-mode');
+            localStorage.setItem("colorTheme", "light-mode");
+        case "light-mode":
+            document.documentElement.classList.remove('light-mode');
+            localStorage.setItem("colorTheme", "console-mode");
+            break;
+        case "console-mode":
+            document.documentElement.classList.remove('console-mode');
+            localStorage.setItem("colorTheme", "");
+            break;
+        default:
+            console.log("Could not load website theme.")
     }
 }
