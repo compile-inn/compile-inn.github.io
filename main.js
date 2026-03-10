43e670a824148602eede1d1764035e426d106d1e
lo <215226467+compile-inn@users.noreply.github.com>
2026-03-10 22:46:40 +0900

    main.js added

---
+++ main.js
@@ -0,0 +1,6 @@
+import { ohMyTrees } from "./fractalTree.js";
+import { lightSwitch } from "./lightSwitch.js";
+
+// Make functions available globally
+window.ohMyTrees = ohMyTrees;
+window.lightSwitch = lightSwitch;
