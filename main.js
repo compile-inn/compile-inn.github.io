f390a03cf26868f0a1c9ef57ad5de894dac0d81d
lo <215226467+compile-inn@users.noreply.github.com>
2026-07-25 22:50:04 +0900

    time online edit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { ohMyTrees } from "./fractalTree.js";
 import { lightSwitch } from "./lightSwitch.js";
+import { onlineSince } from "./timeOnline.js";
 
 // Make functions available globally
 window.ohMyTrees = ohMyTrees;
 window.lightSwitch = lightSwitch;
+window.onlineSince = onlineSince;
